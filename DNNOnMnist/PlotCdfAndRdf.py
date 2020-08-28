26094688271aac2853a3ce18bc09ebcce9cd827a
Johnzhx <70248466+Johnzhx@users.noreply.github.com>
2020-08-28 19:21:33 +0000

    Update PlotCdfAndRdf.py

--- DNNOnMnist/PlotCdfAndRdf.py
+++ DNNOnMnist/PlotCdfAndRdf.py
@@ -1,6 +1,5 @@
-# !python3
-# -*- coding: utf-8 -*-
-# author: flag
+###this program is similar to the one in Resnet18###
+###import the module###
 import sys
 sys.path.insert(0,'D:/GDrive/jupyterNB/basicfolder')
 from BasicFunc import mySaveFig
@@ -8,13 +7,15 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-#%%
+
+###iii*100 represents for the epochs###
 for iii in range(15):
     RR={}
     Seq=[]
     
-    target_folder=r'D:\跟着许志钦搞科研\验证低频比问题\deepstudy20200606 mnist/'
-     
+    target_folder=r'D:\deepstudy20200606 mnist/' #this is your target folder, and is changed with different location.
+    
+    ###find the LFR we have saved, and put them into dictionary RR, for computing CDF and RDF###
     all_sub=os.listdir(target_folder)
     print(all_sub)
     for i in all_sub:
@@ -23,7 +24,6 @@ for iii in range(15):
         fd = target_folder+'/'+ i
         all_sub_i = os.listdir(fd)
         print(all_sub_i)
-        #print(all_sub_i)
         for j in all_sub_i:
             fdd = fd +'/'+ j
             all_sub_ij = os.listdir(fdd)
@@ -33,9 +33,9 @@ for iii in range(15):
                     continue
                 with open(fdd + '/' + k,'rb') as f:
                     R = pickle.load(f)
-                #print(fdd + '/' + k)
                 if R['s_filter_wid'][0]<1:
                     continue
+                ###we use the 5-hidden-layers network, so we have to make sure that we use the right one###
                 if np.shape(R['hidden_units'])[0] != 5: 
                     continue
                 if R['s_filter_wid'] not in Seq:
@@ -67,6 +67,8 @@ for iii in range(15):
     LFR_layer5t4=[]
     LFR_layer55=[]
     LFR_layer5t5=[]
+    
+    ###compute for the CDF and RDF###
     for i in Seq:
         delta_range_plott.append(1/i[0])
         LFR_layer5t2.append(RR['R[s_filter_wid]'+str(i)]['ratio_of_5layers_of_2ndlast'])
@@ -94,48 +96,32 @@ for iii in range(15):
     for l in range(len(Seq)-1):
         LFR_layer55.append((RR['R[s_filter_wid]'+str(Seq[l])]['ratio_of_5layers_of_5thlast']-RR['R[s_filter_wid]'+str(Seq[l+1])]['ratio_of_5layers_of_5thlast'])/(1/Seq[l][0]-1/Seq[l+1][0]))
     
-    #print(LFR_layer3t1)
-    #print(LFR_layer4t1)
-    #print(LFR_layer5t1)
-    #print(LFR_layer6t1)
-    #print(LFR_layer7t1)
-    #print(delta_range_plot)
-    
+    ###plot the CDF###
     plt.figure()
     ax = plt.gca()
-    #plt.plot(all_m,all_Q_ini,'k*',label='Q_ini')
     plt.plot(delta_range_plott,LFR_layer5t1,'r.-',label='layer5',linewidth=0.8)
     plt.plot(delta_range_plott,LFR_layer5t2,'g.-',label='layer4',linewidth=0.8)
     plt.plot(delta_range_plott,LFR_layer5t3,'b.-',label='layer3',linewidth=0.8)
     plt.plot(delta_range_plott,LFR_layer5t4,'c.-',label='layer2',linewidth=0.8)
     plt.plot(delta_range_plott,LFR_layer5t5,'m.-',label='layer1',linewidth=0.8)
     plt.text(0.65,0.6,'epoches = %s00'%(iii),size=12)
-    #plt.title('5_layers_DNN Different_layers_LFR_distribution_function v.s. 1/delta',fontsize=10)        
-    #ax.set_yscale('log')
     plt.xlabel(r'$1/\delta$',fontsize=22)
     plt.ylabel(r'LFR',fontsize=22)
-    #ax.set_xscale('log')
-    #plt.ylim([0.5,1.1])
-    #plt.ylim([0.31,0.32])
     plt.legend(fontsize=12) 
     fntmp = '%s 5 layers DNN Different layers LFR distribution function'%(target_folder) + str(iii)
     mySaveFig(plt,fntmp,ax=ax,isax=1,iseps=0)
     
+    ###plot the RDF###
     plt.figure()
     ax = plt.gca()
-    #plt.plot(all_m,all_Q_ini,'k*',label='Q_ini')
     plt.plot(delta_range_plot,LFR_layer51,'r.-',label='layer5',linewidth=0.8)
     plt.plot(delta_range_plot,LFR_layer52,'g.-',label='layer4',linewidth=0.8)
     plt.plot(delta_range_plot,LFR_layer53,'b.-',label='layer3',linewidth=0.8)
     plt.plot(delta_range_plot,LFR_layer54,'c.-',label='layer2',linewidth=0.8)
-    plt.plot(delta_range_plot,LFR_layer55,'m.-',label='layer1',linewidth=0.8)
-    #plt.title('5_layers_DNN Different_layers_LFR_density_function v.s. 1/delta',fontsize=10)        
+    plt.plot(delta_range_plot,LFR_layer55,'m.-',label='layer1',linewidth=0.8)      
     plt.text(0.59,1.15,'epoches = %s00'%(iii),size=12)    #ax.set_yscale('log')
     plt.xlabel(r'$1/\delta$',fontsize=22)
     plt.ylabel(r'RDF',fontsize=22)
-    #ax.set_xscale('log')
-    #plt.ylim([0.5,1.1])
-    #plt.ylim([0.31,0.32])
     plt.legend(fontsize=12) 
     fntmp = '%s 5 layers DNN  Different layers LFR density function'%(target_folder) + str(iii)
     mySaveFig(plt,fntmp,ax=ax,isax=1,iseps=0)
